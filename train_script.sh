63417529a076ea92534970cfd46b698c673448b5
destinyls <1491078277@qq.com>
2022-11-07 14:19:52 +0800

    LID log

--- train_script.sh
+++ train_script.sh
@@ -1,3 +1,3 @@
 export TORCH_HOME=./$TORCH_HOME
 python exps/bev_depth_lss_r50_256x704_128x128_24e.py --amp_backend native -b 2 --gpus 8
-python exps/bev_depth_lss_r50_256x704_128x128_24e.py --ckpt outputs/bev_depth_lss_r50_256x704_128x128_24e_ema/checkpoints/ -e -b 2 --gpus 8
+python exps/bev_depth_lss_r50_256x704_128x128_24e.py --ckpt outputs/bev_depth_lss_r50_256x704_128x128_24e/checkpoints/ -e -b 2 --gpus 8
